1d3e37e868ad2483cf8fe02c317f115c0903cd83
BÁ ANH <you@example.com>
2024-03-18 22:54:46 +0700

    xong giao dien login

--- src/Pages/Login/login.tsx
+++ src/Pages/Login/login.tsx
@@ -1,15 +1,57 @@
 import React from 'react'
+import styles from './login.module.scss'
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Link } from 'react-router-dom';
 
+
+const form = yup.object({
+    username: yup.string().required("The email address or mobile number you entered isn't connected to an account. Find your account and log in."),
+    password: yup.string().required(),
+}).required();
+
+interface typeForm {
+    username: string;
+    password: string;
+}
 export default function Login() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(form),
+    });
+    const onSubmit = (data: typeForm) => {
+        console.log(data);
+    };
     return (
-        <section className="bg-slate-200 relative grid grid-cols-2">
-            <div className="md:col-span-1 col-span-2"></div>
-            <div className="md:col-span-1 absolute ">
-                <form action="">
-                    <label htmlFor="username"></label>
-                    <input type="text" id='username' placeholder='User Name' />
-                </form>
+        <>
+            <div className="py-10">
+                <Link to="/" className='w-full flex justify-center pb-5'>
+                    <img src="public/logo-dark.png" alt="" className='lg:w-1/12 w-1/4' />
+                </Link>
+                <section className="grid grid-cols-5 gap-2 container mx-auto">
+                    <div className={`hidden lg:flex col-span-5 lg:col-span-3 w-full ${styles.left}`}>
+                    </div>
+                    <div className="col-span-5 lg:col-span-2  w-full flex justify-center items-center pb-12">
+                        <form action="" onSubmit={handleSubmit(onSubmit)} className={`${styles.form}`}>
+                            <input type="text" id='username' {...register("username")} placeholder='User Name' className={styles.input} />
+                            {errors.username && <p className="text-red-500 text-sm">{errors.username.message}</p>}
+                            <input type="password" id="password" placeholder='Password' {...register("password")} className={styles.input} />
+                            <button className={`bg-orange-500 ${styles.button}`}>Log in</button>
+                            <div className="h-px bg-orange-500"></div>
+                            <div className='w-full flex justify-center p-4'>
+                                <img src="public/signature.png" alt="" />
+                            </div>
+                        </form>
+                    </div>
+                    <div className={`lg:hidden flex col-span-5 lg:col-span-3 w-full ${styles.left}`}>
+                    </div>
+                </section>
             </div>
-        </section>
+        </>
+
     )
 }
